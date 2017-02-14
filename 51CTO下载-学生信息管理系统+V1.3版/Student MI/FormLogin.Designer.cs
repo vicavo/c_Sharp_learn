c3eafd3a67d81d8cd80c236aeb258406585a2e0c
vicavo <511964793@qq.com>
2017-02-14 09:49:26 +0800

    update
    new

--- 51CTO下载-学生信息管理系统+V1.3版/Student MI/FormLogin.Designer.cs
+++ 51CTO下载-学生信息管理系统+V1.3版/Student MI/FormLogin.Designer.cs
@@ -41,56 +41,56 @@
             // txtUser
             // 
             this.txtUser.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtUser.Location = new System.Drawing.Point(594, 276);
+            this.txtUser.Location = new System.Drawing.Point(594, 299);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(220, 21);
+            this.txtUser.Size = new System.Drawing.Size(220, 20);
             this.txtUser.TabIndex = 0;
             // 
             // txtPassword
             // 
             this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtPassword.Location = new System.Drawing.Point(594, 323);
+            this.txtPassword.Location = new System.Drawing.Point(594, 350);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(220, 21);
+            this.txtPassword.Size = new System.Drawing.Size(220, 20);
             this.txtPassword.TabIndex = 1;
             // 
             // pbTime
             // 
-            this.pbTime.Location = new System.Drawing.Point(173, 437);
+            this.pbTime.Location = new System.Drawing.Point(173, 473);
             this.pbTime.Name = "pbTime";
-            this.pbTime.Size = new System.Drawing.Size(674, 19);
+            this.pbTime.Size = new System.Drawing.Size(674, 21);
             this.pbTime.TabIndex = 4;
             // 
             // lb_btnLogin
             // 
             this.lb_btnLogin.AutoSize = true;
             this.lb_btnLogin.BackColor = System.Drawing.Color.Transparent;
-            this.lb_btnLogin.Location = new System.Drawing.Point(599, 393);
+            this.lb_btnLogin.Location = new System.Drawing.Point(599, 426);
             this.lb_btnLogin.Name = "lb_btnLogin";
-            this.lb_btnLogin.Size = new System.Drawing.Size(95, 24);
+            this.lb_btnLogin.Size = new System.Drawing.Size(52, 26);
             this.lb_btnLogin.TabIndex = 5;
             this.lb_btnLogin.Text = "               \r\n               ";
-            this.lb_btnLogin.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lb_btnLogin_MouseMove);
             this.lb_btnLogin.Click += new System.EventHandler(this.lb_btnLogin_Click);
+            this.lb_btnLogin.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lb_btnLogin_MouseMove);
             // 
             // lb_brnExit
             // 
             this.lb_brnExit.AutoSize = true;
             this.lb_brnExit.BackColor = System.Drawing.Color.Transparent;
-            this.lb_brnExit.Location = new System.Drawing.Point(727, 393);
+            this.lb_brnExit.Location = new System.Drawing.Point(727, 426);
             this.lb_brnExit.Name = "lb_brnExit";
-            this.lb_brnExit.Size = new System.Drawing.Size(95, 24);
+            this.lb_brnExit.Size = new System.Drawing.Size(52, 26);
             this.lb_brnExit.TabIndex = 5;
             this.lb_brnExit.Text = "               \r\n               ";
-            this.lb_brnExit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lb_brnExit_MouseMove);
             this.lb_brnExit.Click += new System.EventHandler(this.lb_brnExit_Click);
+            this.lb_brnExit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lb_brnExit_MouseMove);
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(-50, 589);
+            this.btnLogin.Location = new System.Drawing.Point(-50, 638);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(51, 26);
+            this.btnLogin.Size = new System.Drawing.Size(51, 28);
             this.btnLogin.TabIndex = 6;
             this.btnLogin.Text = "登录";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -99,9 +99,9 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(-52, 565);
+            this.btnCancel.Location = new System.Drawing.Point(-52, 612);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(51, 26);
+            this.btnCancel.Size = new System.Drawing.Size(51, 28);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "退出";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -110,11 +110,11 @@
             // FormLogin
             // 
             this.AcceptButton = this.btnLogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(1005, 618);
+            this.ClientSize = new System.Drawing.Size(1005, 670);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lb_brnExit);
